ed5d171337d8c67797644ca159658f9f4a091236
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2023-07-29 14:27:34 +0530

    app component files

---
+++ src/context.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const AppContext = createContext();
+
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme:dark)'
+  ).matches;
+  const storedDarkMode = localStorage.getItem('darkTheme') === 'true';
+  return storedDarkMode || prefersDarkMode;
+};
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  const [searchTerm, setSearchTerm] = useState('cat');
+
+  const toggleDarkTheme = () => {
+    const newIsDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newIsDarkTheme);
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-theme', newIsDarkTheme);
+    // console.log(body);
+    localStorage.setItem('darkTheme', newIsDarkTheme);
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme);
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
